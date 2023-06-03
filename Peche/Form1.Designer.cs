5f66facd4cc2b0ccd521b1fc27e0af27a4c48b2f
LeConjuror <leconjuror@outlook.com>
2023-06-02 23:21:13 -0400

    beep boop

--- Peche/Form1.Designer.cs
+++ Peche/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Peche
+﻿using System.Windows.Forms;
+
+namespace Peche
 {
     partial class Form1
     {
@@ -28,21 +30,105 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(169, 203);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(43, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.Font = new System.Drawing.Font("DS-Digital", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(-48, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(483, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Press Hotkey to Start";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.Font = new System.Drawing.Font("DS-Digital", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(-45, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(482, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Interact Key: ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("DS-Digital", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(127, 94);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(130, 26);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "HOTKEY: F2";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Font = new System.Drawing.Font("DS-Digital", 13.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.ForeColor = System.Drawing.Color.White;
+            this.checkBox2.Location = new System.Drawing.Point(127, 129);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(125, 26);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Beep Boop";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(381, 450);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(382, 253);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.MinimumSize = new System.Drawing.Size(400, 300);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Peche Automatique";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
     }
 }
 
--- Peche/Form1.cs
+++ Peche/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading;
@@ -12,22 +13,31 @@ namespace Peche
         private static extern IntPtr ImageSearch(int x, int y, int right, int bottom, [MarshalAs(UnmanagedType.LPStr)] string imagePath);
 
         [DllImport("user32.dll")]
+        static extern short GetKeyState(Keys vKeys);
+
+        [DllImport("user32.dll")]
         static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
 
+        const uint KEYUP = 0x0002;
+        static string dir = Path.GetDirectoryName(Application.ExecutablePath);
 
-        string image_path = Path.Combine(Directory.GetCurrentDirectory(), "image", "interact.png");
         static int[] search_region = { 800, 720, 1000, 760 };
 
-        const byte VK_E = 0x45;
-        const byte VK_F = 0x46;
-        const uint KEYUP = 0x0002;
+        static string intkey;
+        static Dictionary<string, byte> intkeys = new Dictionary<string, byte>(){
+            {"E", 0x45},
+            {"F", 0x46}
+        };
 
-        static byte peche_hotkey = VK_E;
-        static byte interact_key = VK_F;
+        static Keys hotkey = Keys.F2;
+        static bool is_fishing = false;
+        static bool beep = true;
+         
 
         bool searchImage(int[] region)
         {
-            IntPtr result = ImageSearch(region[0], region[1], region[2], region[3], "*Trans0 " + image_path);
+            string image_path = Path.Combine(dir, "image", intkey + ".png");
+            IntPtr result = ImageSearch(region[0], region[1], region[2], region[3], image_path);
             var res = Marshal.PtrToStringAnsi(result);
             if (res != null && res[0] != '0')
             {
@@ -40,14 +50,38 @@ namespace Peche
         {
             while (true)
             {
-                if (searchImage(search_region))
+                if (is_fishing)
                 {
-                    Console.Beep(440, 100);
-                    keybd_event(interact_key, 0, 0, 0);
-                    Thread.Sleep(1000);
-                    keybd_event(interact_key, 0, KEYUP, 0);
+                    if (searchImage(search_region))
+                    {
+                        if (beep)
+                        {
+                            Console.Beep(440, 100);
+                        } 
+                        byte key = intkeys[intkey];
+                        keybd_event(key, 0, 0, 0);
+                        Thread.Sleep(1000);
+                        keybd_event(key, 0, KEYUP, 0);
+                    }
+                }
+                if (GetKeyState(hotkey) < 0)
+                {
+                    checkBox1.Invoke((MethodInvoker)delegate {
+                        checkBox1.Checked = !checkBox1.Checked;
+                    });
+                    Thread.Sleep(500);
                 }
             }
+        }
+
+        void comboBox_init()
+        {
+            foreach (var intkey in intkeys)
+            {
+                comboBox1.Items.Add(intkey.Key);
+            }
+            comboBox1.SelectedIndex = 0;
+            intkey = comboBox1.SelectedItem.ToString();
         }
 
         public Form1()
@@ -57,8 +91,55 @@ namespace Peche
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            comboBox_init();
             Thread FISH = new Thread(peche) { IsBackground = true };
             FISH.Start();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            intkey = comboBox1.SelectedItem.ToString();
+            Console.WriteLine(intkey);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                is_fishing = true;
+                label1.Text = "Fishing in Progress";
+                label1.ForeColor = System.Drawing.Color.Aqua;
+                if (beep)
+                {
+                    Console.Beep(261, 70);
+                    Console.Beep(329, 70);
+                    Console.Beep(392, 70);
+                }
+                
+            } else
+            {
+                is_fishing = false;
+                label1.Text = "Pas de Peche :(";
+                label1.ForeColor = System.Drawing.Color.Red;
+                if (beep)
+                {
+                    Console.Beep(392, 70);
+                    Console.Beep(329, 70);
+                    Console.Beep(261, 70);
+                }
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                beep = true;
+            }
+            else
+            {
+                beep = false;
+            }
         }
     }
 }
